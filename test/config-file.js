06b9a3d517aeb75f3523e93d09c8e7614df74571
Kashi Samaraweera <kashi@kashmaniac.com.au>
2015-11-09 22:38:22 +0100

    Modified sole unit test to return a positive result.

--- test/config-file.js
+++ test/config-file.js
@@ -5,22 +5,27 @@
  * This suite of tests verifies that the config file contains the correct
  * parameters for testing purposes. This is a unit test that does not verify
  * credentials, etc.
+ * 
+ * For the purposes of this template module, the sample config file is targeted
+ * instead (as a config.json does not exist).
  *
  * @author Kashi Samaraweera <kashi@kashis.com.au>
  */
 describe('Config file checks', function() {
 
-    var CONFIG_FILE_PATH = 'config/config.json';
+    var CONFIG_FILE_PATH = 'config/config.sample.json';
 
-    it("Config file exists in /config/config.json", function(done) {
+    it("Config file exists in /config/config.sample.json", function(done) {
         var fs = require('fs'),
         configFileStats;
 
-        (function() {
-        configFileStats = fs.lstatSync(CONFIG_FILE_PATH);
-        (configFileStats.isFile()).should.be.true;
-        }).should.not.throw();
-
+        (
+            function() {
+                configFileStats = fs.lstatSync(CONFIG_FILE_PATH);
+                (configFileStats.isFile()).should.be.true;
+            }
+        ).should.not.throw();
+        
         done();
     });
 
