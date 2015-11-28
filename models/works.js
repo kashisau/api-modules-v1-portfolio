ec9a127939349c2c246a271c5bf70f1c110856f0
Kashi Samaraweera <kashi@kashmaniac.com.au>
2015-11-28 11:53:24 +0100

    Added testing suite for work item files (YAML), implemented works model file loading.

--- models/works.js
+++ models/works.js
@@ -7,20 +7,109 @@
  * creation of new work).
  * 
  * @author Kashi Samaraweera <kashi@kashis.com.au>
- * @version 0.0.1
+ * @version 0.1.0
  */
-var worksModel = {};
+var worksModel = (function() {
+
+    var worksModelAPI = {};
+
+    var config = require('../config/config.json');
+    
+    var yaml = require('js-yaml');
+    var fs = require('fs');
+    var path = require('path');
 
 /**
  * Retrieves a list of works that have been selected from the database with an
- * optional criteria supplied.
- * @param criteria {*}  A key-value based set of criteria for retrival of work.
+ * optional criteria supplied. Errors raised by the file handling methods
+ * invoked are supressed (the unit tests for this model will verify that each
+ * of the work files are syntactically correct.).
+ * @param {string} worksPath    The directory in which to look for the YAML
+ *                              portfolio files.
+ * @param criteria {*}  (Optional) key-value set of criteria for retrival of 
+ *                      work items. Default {"orderby":{"published": "desc"}}.
  * @return {*}[]    Returns an array of work items and summarised work item
  *                  information. This is an array, sorted according to the
  *                  supplied criteria (or otherwise the default sort order).
  */
-worksModel.listWorks = function(criteria) {
-    return [];
+worksModelAPI.listWorks = function(worksPath, criteria) {
+    var worksPath = config.worksPath,
+        workFiles = fs.readdirSync(worksPath),
+        works = [],
+        filePath,
+        fileName;
+
+    for (var i in workFiles) {
+        fileName = workFiles[i];
+        filePath = path.join(worksPath, fileName);
+
+        if ( ! /\.yaml$/i.test(fileName)) continue;
+        if (/sample\.yaml$/i.test(fileName)) continue;
+
+        try {
+            works.push(worksModelAPI.parseWorkFile('..' + filePath));
+        } catch (fileErr) {}
+    }
+    
+    return works;
 }
+
+/**
+ * Parses a portfolio work YAML file (as found in the `works/` folder) into
+ * a JavaScript object so that it may be used by the application. The YAML
+ * file supplied must follow the structure of the (version controlled) example
+ * `works/sample.md` to create a valid Work object.
+ * @param {string} workFile The file in which to look for the YAML portfolio
+ *                          item details.
+ * @return {Work}   Returns an object of type Work with information about the
+ *                  specific work contained in the markdown file.
+ * @throws {Error}  Thrown if the file was not found with error code
+ *                  works_file_not_found.
+ * @throws {Error}  Thrown if the file could not be parsed with error code
+ *                  works_file_oarse_error.
+ */
+worksModelAPI.parseWorkFile = function(workFile) {
+    var workYaml,
+        parseError;
+    
+    try {
+        workYaml = yaml.safeLoad(fs.readFileSync(workFile, 'utf8'));
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            praseError = new Error("There was an error locating the " + 
+                "file in " + workFile + ".");
+            parseError.code = "works_file_not_found";
+            throw parseError;
+        }
+        parseError = new Error("There was an error parsing the works file " +
+            workFile + ". See internal error.");
+        parseError.innerError = err;
+        throw parseError;
+    }
+    return workYaml;
+};
+
+/**
+ * Used with the Array.filter method to remove elements that don't confrom to 
+ * the expected works YAML filename. This is designed to be used when listing
+ * the contents of a directory and deciding which files should be included in
+ * the list of works. Sample files (*sample.yaml) and non-YAML files (!*.yaml)
+ * will be rejected.
+ * This method does not inspect the contents of the files.
+ * @param {string} fileName The file name that should be assessed for
+ *                          suitability.
+ * @return {bool}   Returns TRUE if the file name is expected to be a works
+ *                  item, FALSE if not.
+ */
+worksModelAPI.fileNameFilter = function(fileName) {
+    if ( ! /\.yaml$/i.test(fileName)) return false
+    if (/sample\.yaml$/i.test(fileName)) return false;
+    
+    return true;
+}
+
+return worksModelAPI;
+    
+})();
 
 module.exports = worksModel;
--- test/works-model.js
+++ test/works-model.js
@@ -9,15 +9,70 @@
  * @version 0.0.1
  */
 
+var fs = require('fs'),
+    worksModel = require("../models/works.js"),
+    config = require("../config/config.json"),
+    should = require('should'),
+    path = require('path');
+
 describe('Works model', function() {
-    var worksModel = require("../models/works.js"),
-        should = require('should'),
-        sqlite = require('sqlite3'),
-        db = new sqlite.Database(':memory:');
-        
-    before("Set up temporary database", );
-    it("Retrieve all works", function(done) {
-        (worksModel.listWorks()).should.be.a.array;
-        done();
-    });
+    it(
+        "Work files exist in the directory specified (config.json)",
+        function(done) {
+            var workFiles = fs.readdirSync(config.worksPath);
+            workFiles = workFiles.filter(worksModel.fileNameFilter);
+            
+            workFiles.should.be.a.array;
+            (worksModel.listWorks()).should.be.a.array;
+            done();
+        }
+    );
+});
+
+describe('Checking work files', function() {
+    var workFiles = fs.readdirSync(config.worksPath);
+    workFiles = workFiles.filter(worksModel.fileNameFilter);
+    
+    for (var i in workFiles) {
+        var fileName = workFiles[i],
+            filePath = path.join(config.worksPath, fileName);
+
+        describe("Verifying `" + filePath + "`",
+            function() {
+                var workItem = worksModel.parseWorkFile(filePath);
+                it("Has a name", function(done) {
+                    workItem.name.should.be.a.string; 
+                    done();
+                });
+                it("Has a work type", function(done) {
+                    workItem.type.should.be.a.string;
+                    done();
+                });
+                it("Has a deployment date", function(done) {
+                    var published = workItem.published,
+                        pubParts = published.split('-'),
+                        year = parseInt(pubParts[0]),
+                        quarter = pubParts[1];
+
+                    workItem.published.should.be.a.string;
+                    year.should.be.an.int;
+                    /^[q][1-4]$/i.test(quarter).should.be.true;
+
+                    done();
+                });
+                it("Has a description", function(done) {
+                    workItem.description.should.be.a.string;
+                    done();
+                });
+                if (typeof(workItem.awards) !== 'undefined')
+                    it(
+                        "Awards correctly formatted",
+                        function(done) {
+                            workItem.awards.should.be.an.array;
+                            done();
+                        }
+                    );
+            }
+        );
+    }
 });
